77e3e6e2cc7c6e2f85cecbb8ef603029e3f06eb2
WillianRRP <willianrenato754@gmail.com>
2025-02-24 22:05:22 -0300

    Update App.js

--- primeiro-app/src/App.js
+++ primeiro-app/src/App.js
@@ -1,39 +1,52 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 function App() {
-const[input, setInput] = useState('');
-const[tarefas, setTarefas] = useState([
-  'Pagar conta de luz',
-  'Pagar conta de agua',
-]);
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([]);
 
+ 
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('@tarefa');
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+  
+  
+  useEffect(() => {
+    if (tarefas.length > 0) {  
+      localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+    }
+  }, [tarefas]);
 
-function handleRegister(e){
-  e.preventDefault();
-  setTarefas([...tarefas,input]);
-  setInput('')
+  function handleRegister(e) {
+    e.preventDefault();
+    
+    if (input.trim() === '') {
+      alert('Digite uma tarefa antes de adicionar!');
+      return;
+    }
 
-}
-
+    setTarefas([...tarefas, input]);
+    setInput('');
+  }
 
   return (
     <div>
-
-      <h1>Cadastro de Trefas</h1>
+      <h1>Cadastro de Tarefas</h1>
       <form onSubmit={handleRegister}>
-        <label>Nome da tarefa:</label><br/>
+        <label>Nome da tarefa:</label><br />
         <input 
-        placeholder="Digite uma tarefa"
-        value={input}
-        onChange={ (e) =>setInput(e.target.value) }
-        /><br/>
+          placeholder="Digite uma tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        /><br />
         <button type="submit">Registrar</button>
       </form>
-      <br></br>
+      <br />
       <ul>
-        {tarefas.map( tarefa =>(
-          <li key={tarefa}>{tarefa}</li>
+        {tarefas.map((tarefa, index) => (
+          <li key={index}>{tarefa}</li>
         ))}
       </ul>
     </div>
